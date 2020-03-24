368aa3cdb91bd976c954f1ee05145d442acb30b0
xinxin <nvhashida@icloud.com>
2020-03-24 03:22:34 +0000

    修复 http 上报内存泄漏

--- http/http.go
+++ http/http.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"hetu-core/dto"
+	"io/ioutil"
 	gohttp "net/http"
 
 	"github.com/conthing/utils/common"
@@ -13,7 +14,13 @@ import (
 func Publish(info *dto.PubHTTPInfo, mJSON []byte) {
 	url := fmt.Sprintf("%s:%d%s", info.Address, info.Port, info.URL)
 	buf := bytes.NewBuffer(mJSON)
-	_, err := gohttp.Post(url, "application/json", buf)
+	resp, err := gohttp.Post(url, "application/json", buf)
+	if err != nil {
+		common.Log.Error("HTTP 上报失败", err)
+		return
+	}
+	defer resp.Body.Close()
+	_, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		common.Log.Error("HTTP 上报失败", err)
 		return
--- router/router.go
+++ router/router.go
@@ -18,11 +18,20 @@ func Run(port int) {
 		v1.GET("/nodes", handler.GetZigbeeNodes)
 		v1.GET("/nodes/:mac", handler.GetNodeLatestMessage)
 		v1.GET("/version", handler.GetVersionInfo)
+
+		// 上报设置
 		v1.POST("/PubHTTP", handler.SetPubHTTP)
 		v1.GET("/PubHTTP", handler.GetPubHTTPInfo)
 		v1.POST("/PubMQTT", handler.SetPubMQTT)
 		v1.GET("/PubMQTT", handler.GetPubMQTTInfo)
+
+		// 指令接受
 		v1.POST("/command", handler.HandleCommand)
+
+		// 可以设置其他
+		// v1.GET("/alias", handler.GetAlias)
+		// v1.POST("/alias", handler.SetAlias)
+
 	}
 	r.Run(fmt.Sprintf(":%d", port))
 }
